f23ebbbac516532a7f61fb3b94a4ca353dae1749
getCurrentThread <wak8835@gmail.com>
2022-02-19 15:42:53 +0900

    Update script.js : 전액을 가져오는 것을 비동기 방식으로 변경

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-var lastNum = 1;
-var sum = 0;
-var regexp = /\B(?=(\d{3})+(?!\d))/g;
-var cashList=[];
-$.ajax({
-            url: '/Cash/GetChargeList',
-            type: 'GET',
-            data: { Page: 1, StartDate: '1990.01.01', EndDate: '2100.12.31'},
-            dataType: 'html',
-            async: false,
-            success: function (data) {
-				var pageNum = $(data).find(".pagination__last")[0].getAttribute("onClick");
-                if(pageNum != null){
-                    lastNum = pageNum.replace(/[^0-9]/g,"");
-                }
-            },
-            error: function (xhr, status, error) {
-                ajaxErrorHandler(xhr, status, error);
-                return;
-            }
-        });
-for(var i=1;i<=lastNum;i++){
-    $.ajax({
-            url: '/Cash/GetChargeList',
-            type: 'GET',
-            data: { Page: i, StartDate: '1990.01.01', EndDate: '2100.12.31'},
-            dataType: 'html',
-            async: false,
-            success: function (data) {
-				$(data).find("td.list__price").each(function(){ 
-					var $cash = $(this)
-					cashList.push($cash.text().replace(/[^0-9]/g,""))
-				});
-            },
-            error: function (xhr, status, error) {
-                ajaxErrorHandler(xhr, status, error);
-                return;
-            }
-        });
-}
-
-cashList.forEach(function(cash){
-    sum += Number(cash);
-});
-
-sum = sum.toString().replace(regexp, ',');
-
-alert("현재까지 "+sum+"원 사용하셨습니다");
+fetch('/Cash/GetChargeList?Page=1&StartDate=1990.01.01&EndDate=2100.12.31')
+    .then((response) => {
+        return response.text();
+    })
+    .then((text) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(text, "text/html");
+        const pageNum = doc.getElementsByClassName("pagination__last")[0].getAttribute("onClick");
+        let lastNum = 1;
+        if(pageNum != null){
+            lastNum = pageNum.replace(/[^0-9]/g,"");
+        }
+        return lastNum;
+    })
+    .then(lastNum => {
+        const promises = [];
+        for(var i = 1; i <= lastNum; i++){
+            promises.push(fetch(`/Cash/GetChargeList?Page=${i}&StartDate=1990.01.01&EndDate=2100.12.31`)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((text) => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(text, "text/html");
+                    const list = doc.getElementsByClassName("list__price");
+                    const prices = [];
+                    for(var j = 0; j < list.length; j++){
+                        var price = list[j].innerText;
+                        if(price != null){
+                            prices.push(Number(price.replace(/[^0-9]/g,"")));
+                        }
+                    }
+                    return prices.reduce((a,b) => a + b, 0);
+                })
+            );
+        }
+        return Promise.all(promises)
+            .then(prices => prices.reduce((a,b) => a + b, 0))
+            .then(total => {
+                console.log(total);
+                alert(`현재까지 ${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원 사용하셨습니다`);
+                return total;
+            });
+    })
+    .catch(err => {
+        console.log(err);
+    });
